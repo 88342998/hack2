b9b72c8347f7110897c76fd64f89eb3b73b57b04
88342998 <59965860+88342998@users.noreply.github.com>
2020-01-23 08:31:17 -0600

    Create Hack2.c

---
+++ Hack2.c
@@ -0,0 +1,24 @@
+
+/*
+  Programmer's Name: John Cerny
+  NUID: 88342998
+  Date: 1/23/20
+  Description of the program: A program that computes a math equation
+*/
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main (void) {  
+
+	// Variable declarations  
+
+	// Program statements  
+
+	
+	
+	return 0;
+
+}
