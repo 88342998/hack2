663b62cc39f362f2e7fa02a96540df2289520dd9
NathanLiew <59965730+NathanLiew@users.noreply.github.com>
2020-01-24 21:02:18 -0600

    Update nhack2.c

--- nhack2.c
+++ nhack2.c
@@ -1,33 +1,20 @@
-d = arccos(sin(ϕ1) sin(ϕ2) + cos(ϕ1) cos(ϕ2) cos(∆)) · R
+/*
+  Programmer's Name: Nathan Liew
+  NUID: 41087562
+  Date: 1/23/20
+  Description of the program: A program that computes distance with origin and destination
+  Collaborators: John Cerny
 
-ϕ1 = latitude[-90 to 90]
-ϕ2 = latitude[-90 to 90]
-∆ = longitude[-180 to 180]
-r = deg / 180· π
-
-
-
-Location Distance
-========================
+======================================================================
+d = arccos (sin(ϕ1) sin(ϕ2) + cos(ϕ1) cos(ϕ2) cos(∆)) · R
+ϕ1=latitude [-90 to 90]
+ϕ2=latitude  [-90 to 90]
+∆= longitude [-180 to 180]
+r =deg/180· π
 Origin: (41.948300, -87.655600)
-	Destination : (40.820600, -96.705600)
-	Air distance is 764.990931 kms
-
-	A = origin(1), B = destination(2) (from paper)
-
-	latitude, longitude
-	========================
-	ϕ1 = latitude = 41.948300
-	ϕ2 = latitude = 40.820600
-	∆ = longitude - 96.705600 - -87.655600
-	all(r = deg / 180· π)
-	R=, 6, 371 kilometers
-
-	d = arccos(sin(ϕ1) sin(ϕ2) + cos(ϕ1) cos(ϕ2) cos(∆)) · R
-
-	========================
-
-
+Destination: (40.820600, -96.705600)
+lat 1,lat2, long1,long2 (41.948300, 40.82060,  -87.65560 -96.70560)
+*/
 
 
 #include <stdio.h>
@@ -58,18 +45,18 @@ Radius= 6371;
 
 
 /* d = arccos (sin(ϕ1) sin(ϕ2) + cos(ϕ1) cos(ϕ2) cos(∆)) · R*/
-/* distance = arccos (sin(rad1) sin(rad2) + cos(rad1) cos(rad2) cos(dif) · Radius */
+/* distance = (arccos ((sin(rad1) sin(rad2) + cos(rad1) cos(rad2) cos(dif)) )· Radius */
 /* distance = a + b. */
 
-double distance, a, b,c ;
-a= sin (rad1)*sin (rad2) ;
-c= acos(a);
-b= cos(rad1) * cos(rad2) * cos(dif) * (Radius) ; 
-distance= c+b ;
+double distance, a, b  ;
+a= (sin (rad1)*sin (rad2)) ;
+b= cos(rad1) * cos(rad2) * cos(dif)  ;
+distance= acos(a+b) *Radius;
+
 
 double Origin,Destination, AirDistance;
 printf("Location Distance\n");
-printf("===========================\n");
+printf("=================================\n");
 printf("Origin: (%lf,%lf)\n",lat1,long1);
 printf("Destination: (%lf,%lf)\n",lat2,long2);
 printf("Air Distance is %lf kms\n",distance);
